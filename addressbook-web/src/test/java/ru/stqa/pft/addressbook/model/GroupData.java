8fb214289ee910225b59dc64055a20132cd9ef14
Briniel <Mi3@u9He>
2019-08-27 17:02:59 +0400

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,42 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
-    private final String id;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
-        return id != null ? id.equals(groupData.id) : groupData.id == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -44,10 +14,18 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -60,5 +38,31 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
     }
 }
--- addressbook-web/src/test/java/ru/stqa/pft/addressbook/test/GroupCreatTest.java
+++ addressbook-web/src/test/java/ru/stqa/pft/addressbook/test/GroupCreatTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreatTest extends BaseTest {
@@ -12,8 +13,20 @@ public class GroupCreatTest extends BaseTest {
     public void testGroupCreat() throws Exception {
         app.getNavigationHelper().goToGroupsPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().creatGroup(new GroupData("test-5", null, "New group"));
+        GroupData group = new GroupData("test-5", null, "New group");
+        app.getGroupHelper().creatGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(group);
+        int max = 0;
+        for (GroupData g: after) {
+            if (g.getId() > max){
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 }
