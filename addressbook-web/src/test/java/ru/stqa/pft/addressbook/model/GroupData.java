ec2be4f9e17e88e72ae771d2ebea52b6cb9a5176
Briniel <Mi3@u9He>
2019-08-27 16:50:21 +0400

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,13 +4,7 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+    private final String id;
 
     @Override
     public boolean equals(Object o) {
@@ -19,15 +13,38 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
+        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+        return id != null ? id.equals(groupData.id) : groupData.id == null;
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
     }
 
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
