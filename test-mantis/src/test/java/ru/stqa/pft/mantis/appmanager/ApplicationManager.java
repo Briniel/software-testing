d84b4dd15afe18d699e05641f4745e674c8621a3
IvaMA <M.Ivanov@bssys.com>
2019-10-07 11:32:43 +0300

    Лекция 8.2. Подготовка проекта для тестирования MantisBT

---
+++ test-mantis/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private String browser;
+
+    public ApplicationManager(String browser){
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+        if (browser.equals(BrowserType.CHROME)){
+            System.setProperty("webdriver.chrome.driver", properties.getProperty("driver.browserCHROME"));
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.chrome.driver", properties.getProperty("driver.browserFIREFOX"));
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            System.setProperty("webdriver.ie.driver", properties.getProperty("driver.browserIE"));
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseURL"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+}
---
+++ test-mantis/src/test/java/ru/stqa/pft/mantis/test/BaseTest.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.mantis.test;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+public class BaseTest {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
